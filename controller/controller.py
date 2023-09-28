cc621b0e0e4e055c817ebde38c1a87d51b2d2a54
PriyaQuantumicsAI <priya.darshini@quantumics.ai>
2023-09-28 16:38:18 +0100

    dev setup  code fix

--- controller/controller.py
+++ controller/controller.py
@@ -4,7 +4,8 @@
 
 
 import openai
-
+import requests
+import json
 # Creating a class for the controller
 class AppController:
 
@@ -15,11 +16,30 @@ class AppController:
     def jarvis_gen_ai_process_text(self, json_data):
         res_dic = {}
         user_prompt = json_data.get('prompt')
-        result = user_prompt + 'Success'
-        print(result)
-        return result
+        res_dic = {user_prompt : 'Success'}
+        print(res_dic)
+        return res_dic
         
+    # write a function  to extract the project details from the json_data
+    def extract_project_details(self, json_data):
+        project_details = json_data.get('project_details')
+        return project_details
+            
 
-               
 
-        
+    def call_chatgpt_api(prompt):
+        url = 'https://api.openai.com/v1/engines/davinci-codex/completions'
+        headers = {'Content-Type': 'application/json',
+                'Authorization': 'Bearer YOUR_API_KEY'}
+        data = {
+            'prompt': prompt,
+            'max_tokens': 100,
+            'temperature': 0.7,
+            'n': 1,
+            'stop': None
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
